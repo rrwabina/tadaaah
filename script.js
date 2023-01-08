eff665f95f067f0b0614b13f6eab7070e2ced834
rrwabina <romensamuel@gmail.com>
2023-01-08 11:24:32 +0800

    modified changes

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-﻿function deleteCookie(domain, name) {
+﻿const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+})
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+})
+
+function deleteCookie(domain, name) {
   chrome.cookies.remove({ 
     'domain': domain,
     'name': name, });}
